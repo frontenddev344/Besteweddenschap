6f5eb891e0b9321125095a342e557ff75d3162ef
devavology <145644105+devavology@users.noreply.github.com>
2024-04-23 18:15:12 +0530

    last update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,3 +25,30 @@ document.addEventListener("DOMContentLoaded", function() {
       // You can add code here to set a cookie to remember the user's choice
   });
 });
+$(document).ready(function() {
+  $(".set > a").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".content")
+        .slideUp(200);
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    } else {
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $(this)
+        .find("i")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+      $(".set > a").removeClass("active");
+      $(this).addClass("active");
+      $(".content").slideUp(200);
+      $(this)
+        .siblings(".content")
+        .slideDown(200);
+    }
+  });
+});
